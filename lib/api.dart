398faaf8ff8197cf47d45aca5a1c8ce8c82a3a24
annsfrn <151833915+annsfrn@users.noreply.github.com>
2024-10-25 13:05:11 +0700

    Add files via upload

---
+++ lib/api.dart
@@ -0,0 +1,12 @@
+class BaseUrl {
+  static String tambah =
+      "http://192.168.0.103/sekolah/controller/insert.php";
+  static String lihat =
+      "http://192.168.0.103/sekolah/controller/details.php";
+  static String edit =
+      "http://192.168.0.103/sekolah/controller/update.php";
+  static String hapus =
+      "http://192.168.0.103/sekolah/controller/delete.php";
+  static String data =
+      "http://192.168.0.103/sekolah/controller/list.php";
+}
